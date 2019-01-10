4c687be771b549aa5218b9635f90f66a2f576dc2
haojiong <chj,1229@gmail.com>
2019-01-10 23:50:31 +0800

    LinkedListQueue

---
+++ src/LeetCode1Solution.java
@@ -0,0 +1,21 @@
+public class LeetCode1Solution {
+    public ListNode removeElements(ListNode head, int val) {
+
+        if (head == null) {
+            return null;
+        }
+        head.next = removeElements(head.next, val);
+        return head.val == val ? head.next : head;
+
+    }
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 6, 3, 4, 5, 6};
+        ListNode head = new ListNode(nums);
+        System.out.println(head);
+
+        ListNode res = (new LeetCode1Solution()).removeElements(head, 6);
+        System.out.println(res);
+
+    }
+}
