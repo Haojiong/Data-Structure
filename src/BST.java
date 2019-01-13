aa7d6ea8931b014d2cccfdfbe16c4c3724d27cfc
haojiong <chj,1229@gmail.com>
2019-01-13 23:41:49 +0800

    BST

--- src/BST.java
+++ src/BST.java
@@ -1,3 +1,8 @@
+import java.util.Stack;
+import java.util.Queue;
+import java.util.LinkedList;
+
+
 public class BST<E extends Comparable<E>> {
 
     private class Node {
@@ -28,15 +33,151 @@ public class BST<E extends Comparable<E>> {
     }
 
     public void add(E e) {
-        if (root == null) {
-            root = new Node(e);
+        root = add(root, e);
+    }
+
+    //用递归方法向二分搜索树添加元素
+    private Node add(Node node, E e) {
+        if (node == null) {
             size++;
+            return new Node(e);
+        }
+
+        if (e.compareTo(node.e) < 0) {
+            node.left = add(node.left, e);
+        } else if (e.compareTo(node.e) > 0) {
+            node.right = add(node.right, e);
+        }
+
+        return node;
+    }
+
+    public boolean contains(E e) {
+        return contains(root, e);
+    }
+
+    //递归方法查询二分搜索树中的元素
+    private boolean contains(Node node, E e) {
+        if (node == null) {
+            return false;
+        }
+
+        if (node.e.equals(e)) {
+            return true;
+        } else if (e.compareTo(node.e) < 0) {
+            return contains(node.left, e);
         } else {
-            add(root, e);
+            return contains(node.right, e);
         }
     }
 
-    private void add(Node node, E e){
-        
+    public void preOrder() {
+        preOrder(root);
+    }
+
+    //递归方法前序遍历
+    private void preOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        System.out.println(node.e);
+        preOrder(node.left);
+        preOrder(node.right);
+    }
+
+    //非递归方法前序遍历
+    public void preOrderNR() {
+        Stack<Node> stack = new Stack<>();
+        stack.push(root);
+
+        while (!stack.isEmpty()){
+            Node cur = stack.pop();
+            System.out.println(cur.e);
+
+            if (cur.right!=null){
+                stack.push(cur.right);
+            }
+            if (cur.left!=null){
+                stack.push(cur.left);
+            }
+        }
+    }
+
+
+    public void inOrder() {
+        inOrder(root);
+    }
+
+    //递归方法中序遍历
+    private void inOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        inOrder(node.left);
+        System.out.println(node.e);
+        inOrder(node.right);
+    }
+
+    public void postOrder() {
+        postOrder(root);
+    }
+
+    //递归方法中序遍历
+    private void postOrder(Node node) {
+        if (node == null) {
+            return;
+        }
+
+        postOrder(node.left);
+        postOrder(node.right);
+        System.out.println(node.e);
+    }
+
+    //层序遍历
+    public void levelOrder(){
+       Queue<Node> q = new LinkedList<>();
+       q.add(root);
+
+       while(!q.isEmpty()){
+           Node cur = q.remove();
+           System.out.println(cur.e);
+
+           if(cur.left!=null){
+               q.add(cur.left);
+           }
+           if(cur.right!=null){
+               q.add(cur.right);
+           }
+       }
+    }
+
+    @Override
+    public String toString() {
+        StringBuilder res = new StringBuilder();
+        generateBSTString(root, 0, res);
+        return res.toString();
+    }
+
+    private String generateBSTString(Node node, int depth, StringBuilder res) {
+        if (node == null) {
+            res.append(generateDepthString(depth) + "null\n");
+            return res.toString();
+        } else {
+            res.append(generateDepthString(depth) + node.e + "\n");
+        }
+
+        generateBSTString(node.left, depth + 1, res);
+        generateBSTString(node.right, depth + 1, res);
+        return res.toString();
+    }
+
+    private String generateDepthString(int depth) {
+        StringBuilder res = new StringBuilder();
+        for (int i = 0; i < depth; i++) {
+            res.append("--");
+        }
+        return res.toString();
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -108,7 +108,23 @@ public class Main {
 //        LinkedListStack<Integer> linkedListStack = new LinkedListStack<>();
 //        double time2 = testStack(linkedListStack, optCount);
 //        System.out.println("LinkedListStack, time: " + time2 + "s");
+//        ===========================================================================================================
+        BST<Integer> bst = new BST<>();
+        int[] nums = {5,3,2,4,6,8};
+        for (int num:nums)
+            bst.add(num);
+        bst.preOrder();
+        System.out.println();
+        bst.preOrder();
+        System.out.println();
+        bst.inOrder();
+        System.out.println();
+        bst.postOrder();
+        System.out.println();
+        bst.levelOrder();
 
+        System.out.println();
+        System.out.println(bst);
 
     }
 
